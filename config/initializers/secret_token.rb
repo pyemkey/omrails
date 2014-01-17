10c436ca796d249d6230cc5a5e37bbab19857e8a
pyemkey <py.emkey@gmail.com>
2014-01-17 17:04:47 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = '0f815feab6dc55ce804a5d09bf3b9a0e51fca3e780027975da5edf3a374c0b3d283555b239eccb447ddacf7f6dbda8709b4c652822be37ab73daf16351638012'
