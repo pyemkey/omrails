f31d51f58d8d3c6fbdb461d7f60e366c6715f4c1
pyemkey <py.emkey@gmail.com>
2014-01-19 15:06:16 +0100

    Add validate for user_id

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,6 +4,7 @@ class Pin < ActiveRecord::Base
   has_attached_file :image, styles: { medium:"320x240>"}
 
 	validates_presence_of :description
+	validates_presence_of :user_id
 	validates_attachment :image, presence: true,
 															 content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']},
 															 size: { less_than: 5.megabytes}
