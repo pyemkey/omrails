3443ac5fe2f515b4e6db1955d8ae6bf7da6c3720
pyemkey <py.emkey@gmail.com>
2014-01-19 14:41:33 +0100

    Add Paperclip Gem

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,14 @@
 class Pin < ActiveRecord::Base
-	belongs_to :user
-  attr_accessible :description
-  validates_presence_of :description
+	
+  attr_accessible :description, :image
+  has_attached_file :image, styles: { medium:"320x240>"}
+
+	validates_presence_of :description
+	validates_attachment :image, presence: true,
+															 content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']},
+															 size: { less_than: 5.megabytes}
+
+  belongs_to :user
+
+  
 end
